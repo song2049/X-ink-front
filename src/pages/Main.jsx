78c3548767160111c3e2af296fb77b36407eb6ae
song2049 <song912049@gmail.com>
2025-12-04 18:01:04 +0900

    docs: main페이지 input 적용 테스트

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -4,6 +4,7 @@ import Button from '../components/Buttons/Button';
 import Text from '../components/Texts/Text';
 import Layout from '../layouts/Layout';
 import Container from '../layouts/container';
+import Input from '../components/Inputs/Input';
 
 const StyledMainWrap = styled.div`
   display: flex;
@@ -26,21 +27,25 @@ const Main = () => {
       <Container maxWidth="1260px">
         <StyledMainWrap>
           <Text variant="subtitle">전체 공고 목록</Text>
-          <Dropdown
-            label="버튼 1"
-            options={['전체', '블록체인', '프론트', '백엔드']}
-            onChange={handleDropdownChange}
-          />
-          <Text variant="title">관련 공고들을 모았어요!</Text>
-
-          <div style={{ display: 'flex', gap: '12px', marginTop: '20px' }}>
-            {/* ButtonPlay의 3번 버튼 설정 */}
-            <Button
-              label="바로 가기"
-              variant="blue"
-              size="sm"
-              onClick={handleButton3Click}
+          <div>
+            <Dropdown
+              label="버튼 1"
+              options={['전체', '블록체인', '프론트', '백엔드']}
+              onChange={handleDropdownChange}
             />
+            <Text variant="title">관련 공고들을 모았어요!</Text>
+          </div>
+          <div>
+            <Input variant="search" placeholder="검색어를 입력해주세요." />
+            <div style={{ display: 'flex', gap: '12px', marginTop: '20px' }}>
+              {/* ButtonPlay의 3번 버튼 설정 */}
+              <Button
+                label="바로 가기"
+                variant="blue"
+                size="sm"
+                onClick={handleButton3Click}
+              />
+            </div>
           </div>
           <Text variant="title">Title</Text>
           <Text variant="subtitle">Subtitle</Text>
